01d2b7fa1497dd4279d098aff41bb486edea802b
deanchen <antlet2004@gmail.com>
2021-01-16 11:32:29 +0800

    feat: install libs; can geneate demo map png now;

---
+++ a.test.js
@@ -0,0 +1,33 @@
+var mapnik = require('mapnik');
+var fs = require('fs');
+
+describe("", () => {
+
+  it("", async () => {
+    console.log("ok!");
+
+    // register fonts and datasource plugins
+    mapnik.register_default_fonts();
+    mapnik.register_default_input_plugins();
+
+    var map = new mapnik.Map(256, 256);
+    await new Promise((res, rej) => {
+      map.load('./stylesheet.xml', function(err,map) {
+          if (err) throw err;
+          map.zoomAll();
+          var im = new mapnik.Image(256, 256);
+          map.render(im, function(err,im) {
+            if (err) throw err;
+            im.encode('png', function(err,buffer) {
+                if (err) throw err;
+                fs.writeFile('map.png',buffer, function(err) {
+                    if (err) throw err;
+                    console.log('saved map image to map.png');
+                    res();
+                });
+            });
+          });
+      });
+    });
+  });
+});
