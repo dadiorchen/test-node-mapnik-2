c1989a2eaf1311557d9ebef0036af21dd04bbfb4
deanchen <antlet2004@gmail.com>
2021-01-17 18:57:06 +0800

    feat: generate json for .5m trees, and render them into the png

--- a.test.js
+++ a.test.js
@@ -1,6 +1,9 @@
-var mapnik = require('mapnik');
+const mapnik = require('mapnik');
 const path = require("path");
-var fs = require('fs');
+const fs = require('fs');
+const { Pool, Client } = require("pg");
+const config = require("./config");
+
 
 describe("", () => {
 
@@ -8,10 +11,10 @@ describe("", () => {
     console.log("path:", path.resolve(mapnik.settings.paths.input_plugins, 'geojson.input'));
   });
 
-  it("", async () => {
+  it.only("", async () => {
     console.log("ok!");
-    const scaleH = 10;
-    const scaleV = 5;
+    const scaleH = 200;
+    const scaleV = 100;
 
     // register fonts and datasource plugins
     mapnik.register_default_fonts();
@@ -38,3 +41,66 @@ describe("", () => {
     });
   }, 1000*60);
 });
+
+describe("db", () => {
+  const path = "./trees.geojson";
+
+  it.skip("", async () => {
+    expect(config.connectionString).toMatch(/^post/);
+    const pool = new Pool({ connectionString: config.connectionString });
+    let firstLine = true;
+    fs.writeFileSync(path,`{"type":"FeatureCollection","features":[\n` );
+
+    let offset = 0;
+    const limit = 1000;
+    for(let loopLimit = 0; loopLimit < 600; loopLimit++){
+      query = {
+        text: `SELECT id, lon, lat FROM trees LIMIT ${limit} OFFSET ${offset}`,
+        values: [],
+      };
+      console.log("query:", query);
+      const result = await pool.query(query);
+      //console.log("result:", result);
+      if(result.rows.length > 0){
+        for(let row of result.rows){
+          expect(row).toMatchObject({
+            id: expect.anything(),
+            lon: expect.anything(),
+            lat: expect.anything(),
+          });
+          fs.appendFileSync(path,`${firstLine?"":","}{"type":"Feature","geometry":{"type":"Point","coordinates":[${row.lon},${row.lat}]}}\n`);
+          firstLine = false;
+        }
+      }else{
+        console.log("quit because at the end");
+        break;
+      }
+      offset += limit;
+      console.log("offset arive:", offset);
+      console.log("loopLimit arive:", loopLimit);
+    }
+    fs.appendFileSync(path,`]}`);
+  }, 1000*60*60*2);
+
+  it("check file", () => {
+    const content = fs.readFileSync(path);
+    const lines = content.toString().split(/\n/);
+    console.log("lines:", lines.length);
+    //for(let i = 2; i < lines.length -1; i++){
+    for(let i = 2; i < lines.length -2; i++){
+//,{"type":"Feature","geometry":{"type":"Point","coordinates":[36.84708723798394,-3.3529813773930073]}}
+      if(!lines[i].match(/^,\{"type":"Feature","geometry":\{"type":"Point","coordinates":\[-?\d+(\.\d+)?,-?\d+(\.\d+)?\]\}\}/)){
+        throw `don't match: ${lines[i]}, line: ${i}`;
+      }
+    }
+//    JSON.parse(content.toString());
+  });
+
+  it("check file", () => {
+    const content = fs.readFileSync(path);
+    JSON.parse(content.toString());
+  });
+});
+
+
+
